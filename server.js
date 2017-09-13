4906bc0659ab3b90b91fac66b205ed9c5ed7270d
Vitaliy Kudla <vkudla@scentbird.com>
2017-09-13 11:52:49 +0400

    initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+})
+hbs.registerHelper('capitalize', (text) => {
+	return text.toUpperCase();
+})
+
+hbs.registerPartials(path.join(__dirname, '/views/partials'))
+app.set('view engine', 'hbs')
+
+app.use((req, res, next) => {
+	const nowDate = new Date().toString()
+	const log = `${nowDate}: ${req.method} ${req.url}`
+
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('Unable to append to server.log')
+		}
+	})
+	next()
+})
+
+// app.use((req, res, next) => {
+// 	res.render('main.hbs')
+// })
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home page',
+		welcomeMessage: 'Welcome',
+	})
+})
+
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page',
+	})
+})
+
+app.get('/bad', (req, res) => {
+	res.send({
+		errorMessage: 'Error message'
+	})
+})
+
+app.listen(4000, () => {
+	console.log('Server starting in 4000 port')
+});
